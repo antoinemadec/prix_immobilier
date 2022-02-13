016e5f2a356f1154f2d6ae94c278b725f6e0d050
antoinemadec <ajamadec@gmail.com>
2022-02-13 18:15:02 +0100

    feat: more estimate and input data

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def plot_estimate(dates, y, deg, ax, color="blue"):
+    x = mdates.date2num(dates)
+    z4 = np.polyfit(x, y, deg)
+    p4 = np.poly1d(z4)
+    xx = np.linspace(x.min(), mdates.date2num(DT.date.today()), 100)
+    dd = mdates.num2date(xx)
+    ax.plot(dd, p4(xx), color=color)
+
+
 # arg parsing
 parser = argparse.ArgumentParser(description='plot euros/m2 from csv')
-parser.add_argument('csv_files', nargs='+', help='csv files from https://app.dvf.etalab.gouv.fr/')
+parser.add_argument('csv_files', nargs='+',
+                    help='csv files from https://app.dvf.etalab.gouv.fr/')
 args = parser.parse_args()
 
 # actual data
@@ -22,13 +33,14 @@ for filepath in args.csv_files:
         price = line['valeur_fonciere']
         date = line['date_mutation']
         rue = line['adresse_nom_voie']
+        type = line['type_local']
         try:
             surface = int(float(surface))
             price = int(float(price))
         except:
             continue
-        # if surface < 100 or surface > 200 or rue != "RES DES RANSONNIERES":
-        if surface < 130 or surface > 200:
+        # or rue != "RES DES RANSONNIERES":
+        if type != "Maison":
             continue
         euros_per_m2 = int(price/surface)
         print(f"{date} {price}e {surface}m2 {euros_per_m2}e/m2")
@@ -37,18 +49,12 @@ for filepath in args.csv_files:
         l_euros_per_m2.append(euros_per_m2)
 
 fig, ax = plt.subplots()
-ax.plot(l_date, l_euros_per_m2, 'o', color='tab:brown')
+ax.plot(l_date, l_euros_per_m2, '+', color='orange')
 
 # estimate
-dates = l_date
-y = l_euros_per_m2
-x = mdates.date2num(dates)
-z4 = np.polyfit(x, y, 2)
-p4 = np.poly1d(z4)
-xx = np.linspace(x.min(), x.max(), 100)
-dd = mdates.num2date(xx)
-ax.plot(dd, p4(xx), '-g')
-ax.plot(dates, y, '+', color='b', label='blub')
+plot_estimate(l_date, l_euros_per_m2, 1, ax, color="lightblue")
+plot_estimate(l_date, l_euros_per_m2, 2, ax, color="blue")
+plot_estimate(l_date, l_euros_per_m2, 3, ax, color="darkblue")
 
 # show
 ax.grid()
